a8f428c6ffb69026eb583c49ceaece9d94f5cfa8
Jorge Venegas <brangelina01@gmail.com>
2020-07-22 09:02:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,20 +1,27 @@
 class Doctor
-  attr_reader :name
-  @@all =[]
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
-    @name=name
+    @name = name
     @@all << self
   end
-  def appointments
-    Appointment.all.select {|appt| appt.doctor == self}
-  end
-  def new_appointment(date, patient)
-    Appointment.new(date,patient,self)
-  end
-  def patients
-    self.appointments.map {|appt| appt.patient}
-  end
+
   def self.all
     @@all
   end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    appointments.collect{|appointment| appointment.patient}
+  end
+
 end
