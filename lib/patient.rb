53753123ad95e8f5713907ad49a685284ed5dbd3
Jorge Venegas <brangelina01@gmail.com>
2020-07-22 09:02:50 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,20 +1,27 @@
 class Patient
   attr_accessor :name
-  @@all =[]
+
+  @@all = []
+
   def initialize(name)
-    @name=name
+    @name = name
     @@all << self
   end
+
   def self.all
     @@all
   end
-  def new_appointment(date,doctor)
-    Appointment.new(date,self,doctor)
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
   end
+
   def appointments
-    Appointment.all.select {|appt| appt.patient == self}
+    Appointment.all.select{|appointment| appointment.patient == self}
   end
+
   def doctors
-    self.appointments.map {|appt| appt.doctor}
+      appointments.collect{|appointment| appointment.doctor}
   end
+
 end
